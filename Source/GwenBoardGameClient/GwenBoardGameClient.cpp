f69155f0a9f3d73d82135eee9c9fe30728d6a4e2
DMliangshiming <shimingliang@deepmirror.com>
2023-08-02 15:58:42 +0800

    init

---
+++ Source/GwenBoardGameClient/GwenBoardGameClient.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GwenBoardGameClient.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GwenBoardGameClient, "GwenBoardGameClient" );
---
+++ Source/GwenBoardGameClient/GwenBoardGameClientGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GwenBoardGameClientGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GWENBOARDGAMECLIENT_API AGwenBoardGameClientGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
